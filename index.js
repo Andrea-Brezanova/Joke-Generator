7e373147cb41db9625c37d925c0d1011f0580635
Andrea Brezanova <55324009+Andrea-Brezanova@users.noreply.github.com>
2025-01-25 16:06:12 +0100

    Injected JS PlugIn typewriter.

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function displayJoke(response) {
     // display joke
     console.log("data gathered");
     console.log(response.data.answer);
-    let jokeElement = document.querySelector("#joke");
-    jokeElement.innerHTML = response.data.answer;
+    // let jokeElement = document.querySelector("#joke");
+    // jokeElement.innerHTML = response.data.answer;
+
+    new Typewriter("#joke", {
+      strings:response.data.answer,
+      autoStart: true,
+      cursor: null,
+      delay: 10,
+    });
 }
 
 function createJoke(event) {
@@ -16,12 +23,12 @@ function createJoke(event) {
     let apiKey = "007360a0b0aabc68f2a54boff1b97tfc";
     let context =
       "You are a funny and smart assistant that tells short and sweet jokes."
-    let prompt = "Generate a unique joke. Please do not repeat the joke from the previous prompt. Please provide the output in basic HTML";
+    let prompt = "Generate a unique joke. Please do not repeat the joke from the previous prompt.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let jokeElement = document.querySelector("#joke");
     jokeElement.innerHTML = "Generating a joke for you. You can start smiling already.";
-    
+
     console.log("calling the AI api"),
     axios.get(apiURL).then(displayJoke);
 

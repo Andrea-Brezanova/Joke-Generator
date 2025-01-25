ebc5a5036fb577464f45846e0e68f9cf595f2fd6
Andrea Brezanova <55324009+Andrea-Brezanova@users.noreply.github.com>
2025-01-25 15:45:45 +0100

    Added API and JS functionality to fetch the joke.

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-function showAnswer(response) {
-     alert(response.data.answer);
+function displayJoke(response) {
+    // display joke
+    console.log("data gathered");
+    console.log(response.data.answer);
+    let jokeElement = document.querySelector("#joke");
+    jokeElement.innerHTML = response.data.answer;
 }
 
-let apiKey = "007360a0b0aabc68f2a54boff1b97tfc";
-let context =
-  "please provide a funny joke, make sure to pick one at a time and a new one every time";
-let prompt = "Can you tell me a joke?";
-let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function createJoke(event) {
+    event.preventDefault();
 
-axios.get(apiUrl).then(showAnswer);
+    //let jokeElement = document.querySelector("#joke");
+    //jokeElement.innerHTML = "Funny joke";
 
-let buttonElement = document.querySelector("button");
-buttonElement.addEventListener("click", createJoke)
+    // instead of text above, call the API and inside the callback insert the joke in the box and typewrite it
+    let apiKey = "007360a0b0aabc68f2a54boff1b97tfc";
+    let context =
+      "You are a funny and smart assistant that tells short and sweet jokes."
+    let prompt = "Generate a unique joke. Please do not repeat the joke from the previous prompt. Please provide the output in basic HTML";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let jokeElement = document.querySelector("#joke");
+    jokeElement.innerHTML = "Generating a joke for you. You can start smiling already.";
+    
+    console.log("calling the AI api"),
+    axios.get(apiURL).then(displayJoke);
+
+}
+
+let buttonElement = document.querySelector("#joke-button");
+buttonElement.addEventListener("click", createJoke);
